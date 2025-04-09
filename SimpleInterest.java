16e3f3f42d643480d37e85efd6fedb22105270b6
nisargasg <nisarga.ads@gmail.com>
2025-04-09 16:39:24 +0530

    save java file

---
+++ SimpleInterest.java
@@ -0,0 +1,18 @@
+
+import java.util.Scanner;
+
+public class SimpleInterest {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the principal amount: ");
+        double principal = sc.nextDouble();
+        System.out.println("Enter the rate of interest: ");
+        double rate = sc.nextDouble();
+        System.out.println("Enter the time: ");
+        double time = sc.nextDouble();
+        double interest = (principal * rate * time) / 100;
+        System.out.println("Simple Interest: " + interest);
+    }
+
+}
+
